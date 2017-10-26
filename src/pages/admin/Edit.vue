1847a81ab7925fea43f123959c3818c711ff6e5a
ln <allen.fdes@gmail.com>
2017-10-26 17:39:55 +0530

    children routes added for admin

---
+++ src/pages/admin/Edit.vue
@@ -0,0 +1,21 @@
+<!-- ./src/pages/admin/Edit.vue -->
+<template>
+  <div>
+    <div class="title">
+      <h1>This is Admin/Edit/{{$route.params.id}}</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Edit',
+  data () {
+    return {
+      msg: 'Welcome to the Admin  Edit Page'
+    }
+  },
+  created () {
+    console.log(this.$route.params.id)
+  }
+}
+</script>
